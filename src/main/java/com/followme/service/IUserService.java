0fea564b69f360a3c0b2a14cd5df6248fa7272d3
lusir <1548936488@qq.com>
2018-05-26 16:45:02 +0800

    用户模块 ing

--- src/main/java/com/followme/service/IUserService.java
+++ src/main/java/com/followme/service/IUserService.java
@@ -1,4 +1,27 @@
 package com.followme.service;
 
+import com.followme.common.ServerResponse;
+import com.followme.pojo.User;
+;
+
 public interface IUserService {
+    ServerResponse<User> login(String username, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkValid(String str, String type);
+
+    ServerResponse selectQuestion(String username);
+
+    ServerResponse<String> checkAnswer(String username, String question, String answer);
+
+    ServerResponse<String> forgetRestPassword(String username, String passwordNew, String forgetToken);
+
+    ServerResponse<String> resetPassword(String passwordOld, String passwordNew, User user);
+
+    ServerResponse<User> updateInformation(User user);
+
+    ServerResponse<User> getInformation(Integer userId);
+
+    ServerResponse checkAdminRole(User user);
 }
